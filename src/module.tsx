bffa03873eef4621c95ac35b4f91c210427672e6
Huan Wang <fredwanghuan@gmail.com>
2019-10-11 08:50:32 -0600

    aaa

--- src/module.tsx
+++ src/module.tsx
@@ -1,97 +1,17 @@
-import React, {Component} from 'react';
-import {PanelData, PanelPlugin, PanelProps} from '@grafana/ui';
-import {appEvents} from "grafana/app/core/core";
-import {Container, SelectedIndexProvider, StepChart} from "davi-js"
-import {FieldType} from "@grafana/data";
+import React, { PureComponent } from 'react';
+import { PanelProps, PanelPlugin } from '@grafana/ui';
+import * as reactRedux from 'react-redux';
 
-
-const initialState = {
-    showDocs: false,
-    selectedIndex: 0,
-};
-
-interface PanelState {
-    showDocs: boolean,
-    selectedIndex: number | undefined,
+export class MyPanel extends PureComponent<PanelProps> {
+  render() {
+    return <div>Hello from my panel</div>;
+  }
 }
 
-function transformer(input: PanelData): any {
-    return input.series.map((s) => {
-        if (s.fields.length !== 2) {
-            console.log("something is wrong...", s.fields);
-            return;
-        }
-        const time = s.fields.filter((f) => f.type === FieldType.time)[0].values.toArray();
-        const number = s.fields.filter((f) => f.type === FieldType.number)[0].values.toArray();
+const mapstp = (state: any) => ({
+  ...state,
+});
 
-        const data = time.map((t, i) => {
-            return {x: t, y: number[i]}
-        });
-
-        return {
-            title: s.name,
-            data: data,
-        }
-    })
-}
-
-export class MyPanel extends Component<PanelProps, PanelState> {
-    readonly state = initialState;
-
-    componentDidMount(): void {
-        appEvents.on('graph-hover', (evt: any) => {
-            console.log(evt);
-            const pos = evt.pos;
-            const x = pos.x;
-            this.setState({...this.state, selectedIndex: x})
-        });
-
-        appEvents.on('graph-hover-clear', () => {
-            this.setState({...this.state, selectedIndex: undefined})
-        })
-    }
-
-    onCrosshairHover(x: number | undefined) {
-        if (x) {
-            appEvents.emit('graph-hover', {
-                pos: {
-                    x: x,
-                    x1: x,
-                    y: 1000,
-                    y1: 1000,
-                    panelRelY: 0.5
-                },
-                panel: {id: this.props.id}
-            })
-        } else {
-            appEvents.emit('graph-hover-clear');
-        }
-    }
-
-    render() {
-        const data = transformer(this.props.data);
-        console.log("props", this.props);
-
-        //TODO: configurable title through config panel
-        return (
-            <div className="chart_container">
-                <Container
-                    title={{text: "Davi Yeesss"}}
-                    iconsVisible={true}
-                >
-                    <SelectedIndexProvider value={this.state.selectedIndex}>
-                        {/*
-                    // @ts-ignore}*/}
-                        <StepChart
-                            zoomEnabled={true}
-                            onCrosshairHover={this.onCrosshairHover.bind(this)}
-                            data={data}
-                        />
-                    </SelectedIndexProvider>
-                </Container>
-            </div>
-        );
-    }
-}
+reactRedux.connect(mapstp)(MyPanel);
 
 export const plugin = new PanelPlugin(MyPanel);
--- src/pivotal-ui.d.ts
+++ src/pivotal-ui.d.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
-declare module "pivotal-ui/react/dropdowns" {
-  import { ReactNode } from "react";
-  
+declare module 'pivotal-ui/react/dropdowns' {
+  import { ReactNode } from 'react';
+
   interface IDropdownProps {
     blockingScrim?: boolean; // 	false	If true, blocks mouse events outside of the dropdown. Clicking outside of the dropdown will still close the dropdown.
     buttonAriaLabel?: string; // 	aria-label for the button
@@ -13,18 +13,18 @@ declare module "pivotal-ui/react/dropdowns" {
     icon?: string; // 	'chevron_down'	Name of the icon to use for the toggle icon
     itemClassName?: string; // 	Classname to add to each child li
     link?: boolean; // 	If true, color the dropdown toggle like a link
-    menuAlign?: "none" | "left" | "right"; // Sets the alignment of the menu with the button
+    menuAlign?: 'none' | 'left' | 'right'; // Sets the alignment of the menu with the button
     scroll?: boolean; // 	false	Enables scrolling in the dropdown menu when enabled
     showIcon?: boolean; // 	true	If false, do not render an icon in the dropdown toggle. Icon can not be hidden if split or leaving out title.
-    size?: "normal" | "large" | "small"; // 	Sets the size
+    size?: 'normal' | 'large' | 'small'; // 	Sets the size
     split?: boolean; // 	If true, separates the button text from the toggle
     title?: ReactNode; // 	The button contents
   }
-  
+
   export class Dropdown extends React.Component<IDropdownProps> {}
 }
 
-declare module "pivotal-ui/react/flyout" {
+declare module 'pivotal-ui/react/flyout' {
   interface IFlyoutProps extends React.HTMLAttributes<HTMLInputElement> {
     show: boolean;
     header: string;
@@ -32,7 +32,6 @@ declare module "pivotal-ui/react/flyout" {
     bodyClassName: string;
     onHide: VoidFunction;
   }
-  
+
   export class Flyout extends React.Component<IFlyoutProps> {}
 }
-
