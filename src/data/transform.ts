93733ed211bed8db72a83d1021836333660feea2
Huan Wang <fredwanghuan@gmail.com>
2019-10-11 22:43:26 -0600

    fixing linter issue

---
+++ src/data/transform.ts
@@ -0,0 +1,23 @@
+import { PanelData } from '@grafana/ui';
+import { FieldType } from '@grafana/data';
+
+export function transforme(input: PanelData): any {
+  return input.series.map(s => {
+    if (s.fields.length !== 2) {
+      console.error('something is wrong...', s.fields);
+      return undefined;
+    }
+
+    const tsData = s.fields.filter(f => f.type === FieldType.number)[0].values.toArray();
+    const time = s.fields.filter(f => f.type === FieldType.time)[0].values.toArray();
+
+    const data = time.map((t, i) => {
+      return { x: t, y: tsData[i] };
+    });
+
+    return {
+      title: s.name,
+      data: data,
+    };
+  });
+}
--- src/module.tsx
+++ src/module.tsx
@@ -1,119 +1,91 @@
-import React, {Component} from 'react';
-import {PanelData, PanelPlugin, PanelProps} from '@grafana/ui';
-import {appEvents} from "grafana/app/core/core";
-import {Container, SelectedIndexProvider, StepChart} from "davi-js"
-import {FieldType} from "@grafana/data";
-
+import React, { Component } from 'react';
+import { PanelPlugin, PanelProps } from '@grafana/ui';
+import { appEvents } from 'grafana/app/core/core';
+import { Container, SelectedIndexProvider, StepChart } from 'davi-js';
+import { transforme } from './data/transform';
 
 const initialState = {
-    showDocs: false,
-    selectedIndex: 0,
+  showDocs: false,
+  selectedIndex: 0,
 };
 
 interface PanelState {
-    showDocs: boolean,
-    selectedIndex: number | undefined,
-}
-
-function transformer(input: PanelData): any {
-    return input.series.map((s) => {
-        if (s.fields.length !== 2) {
-            console.log("something is wrong...", s.fields);
-            return;
-        }
-        const time = s.fields.filter((f) => f.type === FieldType.time)[0].values.toArray();
-        const number = s.fields.filter((f) => f.type === FieldType.number)[0].values.toArray();
-
-        const data = time.map((t, i) => {
-            return {x: t, y: number[i]}
-        });
-
-        return {
-            title: s.name,
-            data: data,
-        }
-    })
+  showDocs: boolean;
+  selectedIndex: number | undefined;
 }
 
 export class MyPanel extends Component<PanelProps, PanelState> {
-    readonly state = initialState;
+  readonly state = initialState;
 
-    panelID: number;
-    lastPanel: any;
+  panelID: number;
+  lastPanel: any;
 
-    constructor(props: PanelProps) {
-        super(props);
+  constructor(props: PanelProps) {
+    super(props);
 
-        this.panelID = props.id;
+    this.panelID = props.id;
 
-        this.updateCrosshair = this.updateCrosshair.bind(this);
-        this.clearCrosshair = this.clearCrosshair.bind(this);
+    this.updateCrosshair = this.updateCrosshair.bind(this);
+    this.clearCrosshair = this.clearCrosshair.bind(this);
+  }
+
+  updateCrosshair(evt: any) {
+    console.log(evt);
+    this.lastPanel = evt.panel;
+    const pos = evt.pos;
+    const x = pos.x;
+    this.setState({ selectedIndex: x });
+  }
+
+  clearCrosshair() {
+    this.setState({ selectedIndex: undefined });
+  }
+
+  onCrosshairHover(x: number | undefined) {
+    if (x) {
+      appEvents.emit('graph-hover', {
+        pos: {
+          x: x,
+          x1: x,
+          y: 1000,
+          y1: 1000,
+          panelRelY: 0.5,
+        },
+        panel: { ...this.lastPanel, id: this.panelID },
+      });
+      console.log('sync message emitted');
+    } else {
+      appEvents.emit('graph-hover-clear');
     }
+  }
 
-    updateCrosshair(evt: any) {
-        console.log(evt);
-        this.lastPanel = evt.panel;
-        const pos = evt.pos;
-        const x = pos.x;
-        this.setState({selectedIndex: x});
-    }
+  componentDidMount(): void {
+    appEvents.on('graph-hover', this.updateCrosshair);
+    appEvents.on('graph-hover-clear', this.clearCrosshair);
+  }
 
-    clearCrosshair() {
-        this.setState({selectedIndex: undefined});
-    }
+  componentWillUnmount(): void {
+    appEvents.off('graph-hover', this.updateCrosshair);
+    appEvents.off('graph-hover-clear', this.clearCrosshair);
+  }
 
-    onCrosshairHover(x: number | undefined) {
-        if (x) {
-            appEvents.emit('graph-hover', {
-                pos: {
-                    x: x,
-                    x1: x,
-                    y: 1000,
-                    y1: 1000,
-                    panelRelY: 0.5
-                },
-                panel: {...this.lastPanel, id: this.panelID}
-            })
-            console.log("sync message emitted")
-        } else {
-            appEvents.emit('graph-hover-clear');
-        }
-    }
+  render() {
+    const data = transforme(this.props.data);
+    console.log('props', this.props);
 
-    componentDidMount(): void {
-        appEvents.on('graph-hover', this.updateCrosshair);
-        appEvents.on('graph-hover-clear', this.clearCrosshair);
-    }
-
-    componentWillUnmount(): void {
-        appEvents.off('graph-hover', this.updateCrosshair);
-        appEvents.off('graph-hover-clear', this.clearCrosshair);
-    }
-
-    render() {
-        const data = transformer(this.props.data);
-        console.log("props", this.props);
-
-        //TODO: configurable title through config panel
-        return (
-            <div className="chart_container">
-                <Container
-                    title={{text: "Davi Yeesss"}}
-                    iconsVisible={true}
-                >
-                    <SelectedIndexProvider value={this.state.selectedIndex}>
-                        {/*
+    //TODO: configurable title through config panel
+    return (
+      <div className="chart_container">
+        <Container title={{ text: 'Davi Yeesss' }} iconsVisible={true}>
+          <SelectedIndexProvider value={this.state.selectedIndex}>
+            {/*
                     // @ts-ignore}*/}
-                        <StepChart
-                            zoomEnabled={true}
-                            onCrosshairHover={this.onCrosshairHover.bind(this)}
-                            data={data}
-                        />
-                    </SelectedIndexProvider>
-                </Container>
-            </div>
-        );
-    }
+            <StepChart zoomEnabled={true} onCrosshairHover={this.onCrosshairHover.bind(this)} data={data} />
+          </SelectedIndexProvider>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export const plugin = new PanelPlugin(MyPanel);
